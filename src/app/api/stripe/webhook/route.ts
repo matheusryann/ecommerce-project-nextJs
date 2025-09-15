5ea8867d4270642f0b7c9e378147cf19df437918
Matheus <matheus.ryan@discente.ufma.br>
2025-09-14 21:24:38 -0300

    feat(cart): clear cart after sucessful purchase

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 import { db } from "@/db";
-import { orderTable } from "@/db/schema";
+import { cartItemTable, cartTable, orderTable } from "@/db/schema";
 
 export const POST = async (request: Request) => {
   if (!process.env.STRIPE_SECRET_KEY || !process.env.STRIPE_WEBHOOK_SECRET) {
@@ -27,12 +27,27 @@ export const POST = async (request: Request) => {
     if (!orderId) {
       return NextResponse.error();
     }
-    await db
+    const [order] = await db
       .update(orderTable)
       .set({
         status: "paid",
       })
-      .where(eq(orderTable.id, orderId));
+      .where(eq(orderTable.id, orderId))
+      .returning({
+        userId: orderTable.userId,
+      });
+    if (!order?.userId) {
+      return NextResponse.error();
+    }
+    const [cart] = await db
+      .select({
+        id: cartTable.id,
+      })
+      .from(cartTable)
+      .where(eq(cartTable.userId, order.userId));
+    if (cart) {
+      await db.delete(cartItemTable).where(eq(cartItemTable.cartId, cart.id));
+    }
   }
   return NextResponse.json({ received: true });
 };
